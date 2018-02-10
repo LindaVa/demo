a8fe09fd56e86c8696707f513f65242eddf2c238
liuxj01 <liuxj01@mingyuanyun.com>
2018-02-10 11:52:17 +0800

    css modules

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
 const {injectBabelPlugin} = require('react-app-rewired');
+const rewireCssModules = require('react-app-rewire-css-modules');
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', {libraryName: 'antd-mobile', libraryDirectory: 'es', style: 'css'}], config);
+    config = rewireCssModules(config, env);
     return config;
 };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Button, SearchBar } from 'antd-mobile';
-import css from './css.less';
+import css from './css.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrap page">
+      <div className={css.page}>
         <SearchBar placeholder="搜索" maxLength={8} />
 
       </div>
