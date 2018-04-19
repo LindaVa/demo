f7b752d85c31d7ee1404cef9aa3a04fd0e5174bf
liuxj01 <liuxj01@mingyuanyun.com>
2018-04-19 11:16:33 +0800

    tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Flex, Button, SearchBar, Tabs, List } from 'antd-mobile';
-import css from './css.module.css';
+import { Flex, Button, SearchBar, Tabs, List, Popover, Icon, NavBar } from 'antd-mobile';
+import css from './css/css.module.css';
 
 const tabs = [
     { title: 'First Tab' },
@@ -8,16 +8,30 @@ const tabs = [
     { title: 'Third Tab' },
 ];
 const Item = List.Item;
+const ItemT = Popover.Item;
+const myImg = src => <img src={`https://gw.alipayobjects.com/zos/rmsportal/${src}.svg`} className="am-icon am-icon-xs" alt="" />;
 
 class App extends Component {
+    state = {
+        visible: false,
+        selected: '',
+    };
+    onSelect = (opt) => {
+        this.setState({
+            selected: opt.props.value,
+        });
+    };
+    handleVisibleChange = (visible) => {
+        this.setState({
+            visible,
+        });
+    };
   render() {
     return (
       <div className={css.page}>
         <SearchBar className={css.searchBar} placeholder="搜索" maxLength={8} />
         <div className={css.tabBar}>
-          <Tabs tabs={tabs}
-                initalPage={'t2'}
-          >
+          <Tabs tabs={tabs}>
             <div className={css.contentGroup}>
               <Flex wrap="wrap" className={css.btnFlex}>
                 <Button className={css.btn}>First</Button>
@@ -61,14 +75,52 @@ class App extends Component {
               </List>
             </div>
             <div className={css.contentGroup}>
-
+                <List>
+                    <Item>
+                        <NavBar
+                            mode="light"
+                            rightContent={
+                                <Popover mask
+                                         overlayClassName="fortest"
+                                         overlayStyle={{ color: 'currentColor' }}
+                                         visible={this.state.visible}
+                                         overlay={[
+                                             (<ItemT key="4" value="scan" icon={myImg('tOtXhkIWzwotgGSeptou')} data-seed="logId">Scan</ItemT>),
+                                             (<ItemT key="5" value="special" icon={myImg('PKAgAqZWJVNwKsAJSmXd')} style={{ whiteSpace: 'nowrap' }}>My Qrcode</ItemT>),
+                                             (<ItemT key="6" value="special" icon={myImg('PKAgAqZWJVNwKsAJSmXd')} style={{ whiteSpace: 'nowrap' }}>aaaaa</ItemT>),
+                                             (<ItemT key="7" value="button ct" icon={myImg('uQIYTFeRrjPELImDRrPt')}>
+                                                 <span style={{ marginRight: 5 }}>Help</span>
+                                             </ItemT>),
+                                         ]}
+                                         align={{
+                                             overflow: { adjustY: 0, adjustX: 0 },
+                                             offset: [-10, 0],
+                                         }}
+                                         onVisibleChange={this.handleVisibleChange}
+                                         onSelect={this.onSelect}
+                                >
+                                    <div style={{
+                                        height: '100%',
+                                        padding: '0 15px',
+                                        marginRight: '-15px',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                    }}
+                                    >
+                                        <Icon type="ellipsis" />
+                                    </div>
+                                </Popover>
+                            }
+                        >
+                            气泡
+                        </NavBar>
+                    </Item>
+                </List>
             </div>
           </Tabs>
         </div>
-
       </div>
     );
   }
 }
-
 export default App;
